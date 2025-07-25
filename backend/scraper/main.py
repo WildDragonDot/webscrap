c9dfc09527f96579a4060a51caa28ea255f3980e
codesclown <chandan.vishwakarma@quadbtech.com>
2025-07-26 01:06:57 +0530

    inital: new code pull

--- backend/scraper/main.py
+++ backend/scraper/main.py
@@ -13,15 +13,21 @@ HACKERS_API = "https://dorahacks.io/api/hackathon/wchl25-qualification-round/hac
 session = requests.Session()
 session.headers.update({
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept": "*/*",
     "Accept-Language": "en-US,en;q=0.9",
-    "Referer": "https://dorahacks.io/",
-    "Cache-Control": "max-age=0",
-    "Upgrade-Insecure-Requests": "1",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Referer": "https://dorahacks.io/hackathon/wchl25-qualification-round",
+    "Origin": "https://dorahacks.io",
     "Connection": "keep-alive",
+    "Sec-Fetch-Site": "same-origin",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Ch-Ua": '"Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": '"macOS"',
     "Cookie": "_ga=GA1.1.1669956592.1752489679; cookie:accepted=true; _ga_P3DRZLFQ0Q=GS2.1.s1753469948$o31$g1$t1753469950$j58$l0$h0"
 })
+
 
 
 MAX_RETRIES = 5
